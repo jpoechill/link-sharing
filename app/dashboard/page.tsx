6df3cd91d31e8cd25e2952b4c1510a4ac3abc09e
jpoechill <poamrong@rith.com>
2023-12-23 11:38:58 -0800

    add app state

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -20,15 +20,15 @@ interface StateProperties {
 export default function Home() {
   const [data, setData] = useState<StateProperties[]>([]);
 
-  function BearCounter() {
-    const bears = useStore((state) => state.bears)
-    return <h1>{bears} around here...</h1>
-  }
+  // function BearCounter() {
+  //   const bears = useStore((state) => state.bears)
+  //   return <h1>{bears} around here...</h1>
+  // }
 
-  function Controls() {
-    const increasePopulation = useStore((state) => state.increasePopulation)
-    return <button onClick={increasePopulation}>one up</button>
-  }
+  // function Controls() {
+  //   const increasePopulation = useStore((state) => state.increasePopulation)
+  //   return <button onClick={increasePopulation}>one up</button>
+  // }
 
   const listOptions: Platforms[] = [
     {
